492919eb52bdf638a0d7e4aec603f670b4c943b2
artmast64 <brady.johnsonhill@gmail.com>
2026-08-08 13:53:30 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mis_4480_hw3',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='Homework #3 for MIS 4480',
+    author='Brady Johnson-Hill',
+    license='',
+)
